66e6069304023fa0cb822d5ee3b941725347bd86
Merveille-pharel1 <pharelkamga777@gmail.com>
2025-04-25 12:20:35 -0400

    Ajout communication Arduino-PC

--- maison_intelligente/maison_intelligente.ino
+++ maison_intelligente/maison_intelligente.ino
@@ -1,6 +1,7 @@
 #include <HCSR04.h>
 #include <LiquidCrystal_I2C.h>
 #include <AccelStepper.h>
+#include <U8g2lib.h>
 
 #define PIN_RGB 3
 #define TRIGGER_PIN 12
@@ -10,10 +11,17 @@
 #define IN_2 9
 #define IN_3 10
 #define IN_4 11
+#define CLK_PIN 32
+#define DIN_PIN 30
+#define CS_PIN  34 
+
+#pragma region Variables
 
 AccelStepper myStepper(MOTOR_INTERFACE_TYPE, IN_1, IN_3, IN_2, IN_4);
 LiquidCrystal_I2C lcd(0x27,16,2);
 HCSR04 hc(TRIGGER_PIN,ECHO_PIN);
+U8G2_MAX7219_8X8_F_4W_SW_SPI u8g2(U8G2_R0,CLK_PIN,DIN_PIN,CS_PIN,U8X8_PIN_NONE,U8X8_PIN_NONE);
+
 
 const int rgbPin[3] = {28,26,24};
 const int buzzerPin = 22;
@@ -24,9 +32,13 @@ enum AlarmeState {ACTIVE, DESACTIVE};
 
 enum AppState {NORMAL, TROP_LOIN, TROP_PROCHE};
 
+enum CommandeState {STOP, ERREUR, INCONNUE, CONNUE};
+
 AppState appState = NORMAL;
 
 AlarmeState alarmeState = DESACTIVE;
+
+CommandeState commandeState = STOP;
 
 unsigned long currentTime = 0;
 const int lastValue = 400;
@@ -37,8 +49,8 @@ int angle = 0;
 int minStep;
 int maxStep;
 
-const int minDistance = 30;
-const int maxDistance = 60; 
+int minDistance = 30;
+int maxDistance = 60; 
 const int minAngle = 10;
 const int maxAngle = 170;
 const int fullStep = 2038;
@@ -47,6 +59,10 @@ const int fullAngle = 360;
 const String numero = "2347643";
 String ligne1 = numero;
 String ligne2 = "Labo 4B"; 
+
+#pragma endregion
+
+#pragma region Modèles
 
 void affichageDebut(){
   
@@ -61,8 +77,6 @@ void affichageDebut(){
   ligne2 = "";
   lcd.clear();
 }
-
-#pragma region Modèles
 
 void color (unsigned long now)// the color generating function  
 {
@@ -141,7 +155,7 @@ void normalState(){
 
   int distanceLim = constrain(distance, minDistance, maxDistance); 
   int step = map(distanceLim, minDistance, maxDistance, minStep, maxStep);
-
+  
   if(myStepper.distanceToGo() == 0){
     myStepper.moveTo(step);
   }
@@ -236,6 +250,8 @@ void gererAlarme(unsigned long now){
 
 #pragma endregion
 
+#pragma region management
+
 void stateManager() {
   // Adapter selon votre situation!
   switch (appState) {
@@ -265,6 +281,182 @@ void alertManager(unsigned long currentTime) {
   }
 }
 
+void manageCommande(unsigned long currentTime){
+
+  switch (commandeState) {
+    case ERREUR:
+      errorPixel(currentTime);
+      break;
+    case INCONNUE:
+      inconnuPixel(currentTime);
+      break;
+    case CONNUE:
+      connuPixel(currentTime);
+      break;
+    default:
+      u8g2.clearBuffer();
+      u8g2.sendBuffer();
+      break;  
+  }
+
+}
+
+#pragma endregion
+
+#pragma region configuration
+
+void errorPixel(unsigned long now){
+
+  static bool firstTime = true;
+  static unsigned long lastTime = 0;
+  const int rate = 3000;
+
+  if(firstTime){
+    lastTime = now;
+    firstTime = false;
+  }
+  
+  if(now - lastTime >= rate) {
+    commandeState = STOP;
+    firstTime = true;
+    return;
+  }
+
+  u8g2.drawCircle(3,3,3);
+  u8g2.drawLine(1,1,5,5);
+  u8g2.sendBuffer();
+
+}
+
+void inconnuPixel(unsigned long now){
+
+  static bool firstTime = true;
+  static unsigned long lastTime = 0;
+  const int rate = 3000;
+
+  if(firstTime){
+    lastTime = now;
+    firstTime = false;
+  }
+  
+  if(now - lastTime >= rate) {
+    commandeState = STOP;
+    firstTime = true;
+    return;
+  }
+
+  u8g2.drawLine(0,0,7,7);
+  u8g2.drawLine(7,0,0,7);
+  u8g2.sendBuffer();
+  
+}
+
+void connuPixel(unsigned long now){
+
+  static bool firstTime = true;
+  static unsigned long lastTime = 0;
+  const int rate = 3000;
+
+  if(firstTime){
+    lastTime = now;
+    firstTime = false;
+  }
+  
+  if(now - lastTime >= rate) {
+    commandeState = STOP;
+    firstTime = true;
+    return;
+  }
+
+  u8g2.drawLine(0,3,2,5);
+  u8g2.drawLine(2,5,7,0);
+  u8g2.sendBuffer();
+  
+}
+
+void analyserCommande(const String& tampon, String& commande, String& arg1, String& arg2) {
+  commande = "";
+  arg1 = "";
+  arg2 = "";
+
+  int firstSep = tampon.indexOf(';');
+  int secondSep = tampon.indexOf(';', firstSep + 1);
+
+  if (firstSep == -1) {
+    // Pas de point-virgule, c'est peut-être "stop" ou autre commande sans paramètre
+    commande = tampon;
+    return;
+  }
+
+  // Extraire la commande
+  commande = tampon.substring(0, firstSep);
+
+  // Extraire arg1
+  if (secondSep != -1) {
+    arg1 = tampon.substring(firstSep + 1, secondSep);
+
+    arg2 = tampon.substring(secondSep + 1);
+  } else {
+    // Il y a une seule valeur après la commande
+    arg1 = tampon.substring(firstSep + 1);
+  }
+}
+
+void serialEvent() {
+  String tampon = Serial.readStringUntil('\n');
+
+  if(tampon.length() == 0 ) return;
+
+  Serial.println("Réception : " + tampon);
+
+  String commande;
+  String arg1, arg2;
+
+  analyserCommande(tampon, commande, arg1, arg2);
+
+  if (commande == "gDist") {
+    Serial.println(distance);
+    commandeState = CONNUE;
+  }
+
+  else if (commande == "cfg") {
+
+    commandeState = CONNUE;
+
+    if(arg1 == "alm"){
+      distanceAlarme = arg2.toInt();
+    }
+
+    else if(arg1 == "lim_inf"){
+
+      if(arg2.toInt() < maxDistance){
+        minDistance = arg2.toInt();
+      }
+      else{
+        commandeState = ERREUR;
+      }
+    }
+
+    else if(arg1 == "lim_sup"){
+      if(arg2.toInt() > minDistance){
+        maxDistance = arg2.toInt();
+      }
+      else{
+        commandeState = ERREUR;
+      }
+    }
+    else{
+      commandeState = INCONNUE;
+    }
+  }
+  
+  else{
+    commandeState = INCONNUE;
+  }
+}
+
+#pragma endregion
+
 #pragma region setup-loop
 
 void setup() {
@@ -280,12 +472,18 @@ void setup() {
   lcd.backlight();
   affichageDebut();
 
+  u8g2.begin();
+  u8g2.setContrast(5);
+  
+  u8g2.setFont(u8g2_font_4x6_tr);
+  u8g2.clearBuffer(); 
+  u8g2.sendBuffer();
+
   minStep = minAngle * (fullStep * 1.0 /fullAngle);
   maxStep = maxAngle * (fullStep * 1.0 /fullAngle);
   
   myStepper.setMaxSpeed(1000);  
   myStepper.setAcceleration(250); 
-
   myStepper.moveTo(minStep);
 }
 
@@ -296,10 +494,12 @@ void loop() {
   actualiserAffichage(currentTime);
   retournerDistance(currentTime);
   stateManager();
+
   gererAlarme(currentTime);
   alertManager(currentTime);
-  affichagePort(currentTime);
 
-  myStepper.run();
+  manageCommande(currentTime);
+
+  myStepper.run(); 
 }
 #pragma endregion
